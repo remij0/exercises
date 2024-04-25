6b7314c17d140aff198cb4cda87d0857653848e7
remij0 <145967628+remij0@users.noreply.github.com>
2024-04-25 08:39:36 +0200

    exercise

--- 02-oo/01-classes/09-local-library/student.py
+++ 02-oo/01-classes/09-local-library/student.py
@@ -1,17 +1,28 @@
 class Book:
     def __init__(self, title, author):
-        pass
+        self.title = title
+        self.author = author
 
 
 class Library:
     def __init__(self, name):
-        pass
+        self.name = name
+        self.books = []
 
     def add_book(self, book):
-        pass
+        self.books.append(book)
 
     def remove_book(self, book):
-        pass
+        for library_book in self.books:
+            if book.title == library_book.title and book.author == library_book.author:
+                self.books.remove(book)
 
     def search_books(self, search_string):
-        pass
+        results = []
+        for book in self.books:
+            if (
+                search_string.lower() in book.title.lower() or search_string.lower() in book.author.lower()
+            ):
+                results.append(book)
+
+        return results
